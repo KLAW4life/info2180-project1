882c3c3bb60ec61d75ccd8647b73889ec97a3b56
KLAW4life <kerene789@gmail.com>
2022-10-30 18:02:25 -0500

    Added JS to Newsletter

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener("DOMContentLoaded", function(event){
+
+    let button = document.querySelector("form button");
+
+    button.addEventListener("click", e =>{
+        e.preventDefault();
+        let email = document.getElementById("email").value;
+        let notification = document.getElementsByClassName("message")[0];
+        if (email === "") {
+            notification.innerText = "Please enter a valid email address";     
+        }
+        else{
+            notification.innerText = `Thank you! Your email address ${email}  has been added to our mailing list!`;     
+        }
+    });
+});
